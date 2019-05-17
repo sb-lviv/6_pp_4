73a693348ec52a7602acb540277e7fe5c883d938
Serhii Boiko <serhiy.boiko@plvision.eu>
2019-05-17 17:52:00 +0300

    added validation to archive

--- class/archive.js
+++ class/archive.js
@@ -1,16 +1,47 @@
 
 module.exports = function(mongoose, Teacher, Course, Student) {
   const Schema = new mongoose.Schema({
-    grade: Number,
-    teacher: mongoose.Schema.Types.ObjectId,
-    course:  mongoose.Schema.Types.ObjectId,
-    student: mongoose.Schema.Types.ObjectId,
+    grade: {
+      type: Number,
+      required: [true, 'is required'],
+    },
+    teacher: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, 'is required'],
+      validate: async function(_id) {
+        let [teacher] = await Teacher.find({_id});
+        if (!teacher)
+          throw new Error(`There is no teacher with id ${_id}`);
+      },
+    },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, 'is required'],
+      validate: async function(_id) {
+        let [course] = await Course.find({_id});
+        if (!course)
+          throw new Error(`There is no course with id ${_id}`);
+      },
+    },
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, 'is required'],
+      validate: async function(_id) {
+        let [student] = await Student.find({_id});
+        if (!student)
+          throw new Error(`There is no student with id ${_id}`);
+      },
+    },
   });
   const Archive = mongoose.model('Archive', Schema);
   const ObjectId = mongoose.Types.ObjectId;
 
   async function create(record) {
-    return Archive.create(record);
+    return Archive.create(record)
+      .catch(e => {
+        if (!e.errors) throw e;
+        return {error: e.message};
+      });
   }
 
   async function find_all() {
