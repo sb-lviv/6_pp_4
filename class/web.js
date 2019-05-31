228ee689147f8edc3cfc78c3fb0991cab56361cc
Serhii Boiko <serhiy.boiko@plvision.eu>
2019-05-31 15:03:02 +0300

    added gateway validation

--- class/web.js
+++ class/web.js
@@ -7,6 +7,7 @@ module.exports = function(mongoose) {
   const COURSE  = `http://localhost:${CONFIG.PORT.COURSE }`;
   const STUDENT = `http://localhost:${CONFIG.PORT.STUDENT}`;
   const ARCHIVE = `http://localhost:${CONFIG.PORT.ARCHIVE}`;
+  const ObjectId = mongoose.Types.ObjectId;
 
   /*         */
   /* TEACHER */
@@ -33,7 +34,19 @@ module.exports = function(mongoose) {
   /*  COURSE  */
   /*          */
   router.post('/course', async (req, res) => {
-    request.post({url: COURSE, form: req.body, resolveWithFullResponse: true})
+    if (!ObjectId.isValid(req.body.teacher)) {
+      res.status(400).json({message: `Invalid id ${req.body.teacher}`});
+      return;
+    }
+    let id = req.body.teacher;
+    request.get({url: `${TEACHER}/${id}`, resolveWithFullResponse: true})
+    .then(
+      r => request.post({url: COURSE, form: req.body, resolveWithFullResponse: true}),
+      ({response: {headers: headers}, statusCode, error: body}) => {
+        if (statusCode === 404)
+          return {headers, statusCode, body: {message: `There is no teacher with id ${id}`}};
+        return {headers, statusCode, body};
+      })
     .then(r => res.set(r.headers).status(r.statusCode).send(r.body))
     .catch(r => res.set(r.response.headers).status(r.statusCode).send(r.error));
   });
@@ -59,6 +72,33 @@ module.exports = function(mongoose) {
     .catch(r => res.set(r.response.headers).status(r.statusCode).send(r.error));
   });
 
+  router.post('/student/:id/course', async (req, res) => {
+    let courses = toArray(req.body.course_id);
+    if (courses.length === 0) {
+      return res.status(400).json({message: `Please specify course_id`});
+    }
+    try {
+      for await (let id of courses) {
+        if (!ObjectId.isValid(id)) {
+          return res.status(400).json({message: `Invalid id ${id}`});
+        }
+        await request.get(`${COURSE}/${id}`);
+      }
+    } catch(r) {
+      if (r.statusCode === 404) {
+        res.set(r.response.headers)
+           .status(r.statusCode)
+           .json({message: `There is no course with id ${r.response.request.path}`});
+      } else {
+        res.set(r.response.headers).status(r.statusCode).send(r.error);
+      }
+      return;
+    }
+    request.post({url: `${STUDENT}/${req.params.id}/course`, form: req.body, resolveWithFullResponse: true})
+    .then(r => res.set(r.headers).status(r.statusCode).send(r.body))
+    .catch(r => res.set(r.response.headers).status(r.statusCode).send(r.error));
+  });
+
   router.get('/student', async (req, res) => {
     request.get({url: STUDENT, resolveWithFullResponse: true})
     .then(r => res.set(r.headers).status(r.statusCode).send(r.body))
@@ -75,6 +115,16 @@ module.exports = function(mongoose) {
   /* ARCHIVE */
   /*         */
   router.post('/archive', async (req, res) => {
+    let teacher = request.get(`${TEACHER}/${req.body.teacher}`);
+    let student = request.get(`${STUDENT}/${req.body.student}`);
+    let course  = request.get(`${COURSE }/${req.body.course }`);
+    let r = await Promise.all([teacher, student, course])
+        .catch(r => {
+          res.status(404).json({message: `id ${r.response.request.path} not found`});
+          return null;
+        });
+    if (!r) return;
+
     request.post({url: ARCHIVE, form: req.body, resolveWithFullResponse: true})
     .then(r => res.set(r.headers).status(r.statusCode).send(r.body))
     .catch(r => res.set(r.response.headers).status(r.statusCode).send(r.error));
@@ -92,4 +142,10 @@ module.exports = function(mongoose) {
     .catch(r => res.set(r.response.headers).status(r.statusCode).send(r.error));
   });
   return router;
+}
+
+function toArray(object) {
+  if (object instanceof Array) return object;
+  if (object === undefined) return [];
+  else return [object];
 }
